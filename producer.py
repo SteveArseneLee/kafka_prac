95dfdf1cff7f6650f14e5557cf383a9a833a09ff
SteveArseneLee <lclgood@naver.com>
2022-05-09 21:08:54 +0900

    2

--- producer.py
+++ producer.py
@@ -1,12 +1,12 @@
 import time
 from kafka import KafkaProducer
 from faker import Faker
-
+fake = Faker()
 # producer = KafkaProducer(bootstrap_servers=['localhost:9092,localhost:9093,localhost:9094'])
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
 for _ in range(100):
-    name = Faker.name()
+    name = fake
     producer.send('names', name.encode('utf-8'))
     print(name)
 time.sleep(20)
